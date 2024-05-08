00c4a261f5c3b5b123ff4b622e14efc51d9424fa
James Esguerra <jesguerra@stealthmonitoring.com>
2024-05-08 14:31:52 +0800

    add angular

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddDbContext<DataContext>(opt => {
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// cors
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +24,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => {
+    builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:4200");
+});
 
 app.UseHttpsRedirection();
 
